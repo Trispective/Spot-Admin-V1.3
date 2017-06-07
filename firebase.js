b894dd69a29775d0656b0d5cdb550a2ef937d186
Mohammed <s3496568@student.rmit.edu.au>
2017-06-08 02:08:21 +1000

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBNEuC3Iq_GJjuVoTXbvTxUVcJSioNCVvo",
+    authDomain: "spotrmit.firebaseapp.com",
+    databaseURL: "https://spotrmit.firebaseio.com",
+    projectId: "spotrmit",
+    storageBucket: "spotrmit.appspot.com",
+    messagingSenderId: "698829504204"
+  };
+  firebase.initializeApp(config);
